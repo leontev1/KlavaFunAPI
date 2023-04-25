b357262a1503e5cb96075c1864c9546ea79cc214
leontev1 <dmitryleontev@inbox.ru>
2023-04-26 01:35:19 +0300

    Initial commit

---
+++ src/main/java/com/leontev/klavafunapi/KlavaFunApiApplication.java
@@ -0,0 +1,13 @@
+package com.leontev.klavafunapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlavaFunApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KlavaFunApiApplication.class, args);
+    }
+
+}
